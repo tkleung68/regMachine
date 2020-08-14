b9f8a80e71aa1b64d924b37eb9d87206e7502843
tkleung68 <tkleung68-c@my.cityu.edu.hk>
2020-08-14 20:29:26 +0800

    Add the detail readme for user and changed the time interval

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-reg courses
+For Cityu banner 8 registration
+
+1. install python3 (for my environment 3.7.7)
+2. install the library "selenium" by running the command "pip3 install selenium" on terminal
+3. change the path of chromedriver
+   for example in windows "/Users/user/Desktop/regMachine/chromedriver.exe"
+
+   for example in mac "./chromedriver"
+
+my package includes the chromedriver we need
+you can also download from https://chromedriver.chromium.org/downloads
+(chromedriver.exe for windows, chromedriver for liunx or mac)
+
+4. fill your cityu account in line 7 and line 8
+   (only used for login and will not store )
+
+5. fill in the CRNs at most 10 courses
+   (follow the example: [10121, 10222])
+
+6. change the timeticket
+   (follow the example in line 10 and 11)
--- regMachine.py
+++ regMachine.py
@@ -2,58 +2,62 @@ from selenium import webdriver
 import time
 from datetime import datetime, timedelta
 
-chromedriver = "/Users/user/Desktop/regMachine/chromedriver.exe"
+chromedriver = "./chromedriver"
 driver = webdriver.Chrome(chromedriver)
 acc = ""
 pw = ""
 CRNs = [10121, 10222]
-TIMETICKET = datetime(year=2020,month=5,day=27,hour=15,minute=00,second=00)
+TIMETICKET = datetime(year=2020, month=8, day=24,
+                      hour=11, minute=00, second=00)
+
 
 def login():
     try:
-        driver.get("https://banweb.cityu.edu.hk/pls/PROD/twgkpswd_cityu.P_WWWLogin")
+        driver.get(
+            "https://banweb.cityu.edu.hk/pls/PROD/twgkpswd_cityu.P_WWWLogin")
         searchEid = driver.find_element_by_xpath("//input[@type='text']")
         searchEid.send_keys(acc)
 
-        searchPassword = driver.find_element_by_xpath("//input[@type='password']")
+        searchPassword = driver.find_element_by_xpath(
+            "//input[@type='password']")
         searchPassword.send_keys(pw)
 
         searchLogin = driver.find_element_by_xpath("//input[@type='submit']")
         searchLogin.click()
-        
+
         if(driver.title == 'Personal Information'):
             return True
     except Exception as e:
         print(e)
 
     return False
-
-
-    
-    #driver.find_element_by_id("username").send_keys("USERNAME")
+    # driver.find_element_by_id("username").send_keys("USERNAME")
     #obj = driver.find_element(By.NAME, "IamBanana")
 
 
 def locateToRegistrationPage():
-    #/html/body/div[3]/span/map/table/tbody/tr[1]/td/table/tbody/tr/td[5]/a
+    # /html/body/div[3]/span/map/table/tbody/tr[1]/td/table/tbody/tr/td[5]/a
     driver.find_element_by_link_text("Course Registration").click()
     driver.find_element_by_link_text("Main Menu for Web Add/Drop").click()
     driver.find_element_by_link_text("Add or Drop Classes").click()
     driver.find_element_by_xpath("/html/body/div[5]/form/input").click()
 
+
 def addCourse():
     for i in range(0, len(CRNs)):
         field = driver.find_element_by_id("crn_id" + str(i+1))
         field.send_keys(CRNs[i])
-    buttom = driver.find_element_by_xpath("//input[@type='submit'][@name='REG_BTN'][@value='Submit Changes']")
+    buttom = driver.find_element_by_xpath(
+        "//input[@type='submit'][@name='REG_BTN'][@value='Submit Changes']")
     buttom.click()
+
 
 def wait():
     correctPeriod = False
     while not correctPeriod:
         now = datetime.now()
-        if((now+ timedelta(seconds=30)) >=TIMETICKET):
-            print("enter!")
+        if((now + timedelta(seconds=5)) >= TIMETICKET):
+            print("start entering the reg pages!")
             driver.refresh()
             if("ADD COURSE:" in driver.page_source):
                 correctPeriod = True
@@ -63,10 +67,11 @@ def wait():
                 driver.refresh()
                 print("refreshed")
 
+
 if(login()):
     print("login successfully")
     locateToRegistrationPage()
     wait()
     addCourse()
-    
-
+else:
+    print("wrong acc/pw")
